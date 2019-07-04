b51a610375f017522cb3be0fd48412a6d6c561d5
ANKIT3046 <40267545+ANKIT3046@users.noreply.github.com>
2019-07-04 18:55:04 +0530

    Update datasetgen.py

--- datasetgen.py
+++ datasetgen.py
@@ -1,39 +1,44 @@
 import cv2
+import numpy as np
 import sqlite3
-cam=cv2.VideoCapture(0)
-detector=cv2.CascadeClassifier('classifier/haarcascade_frontalface_default.xml')
-
+face_casecade=cv2.CascadeClassifier('classifier/haarcascade_frontalface_default.xml')
+cap=cv2.VideoCapture(0)
 def insertOrUpdate(Id,Name):
-    conn=sqlite3.connect('FaceBase.db')
-    cmd="SELECT * FROM people WHERE ID="+str(Id)
+    conn=sqlite3.connect('hellodata.db')
+    cmd="SELECT * FROM hello WHERE ID="+str(Id)
     cursor=conn.execute(cmd)
-    isRecordExist=0=
+    isRecordExist=0
     for row in cursor:
         isRecordExist=1
     if(isRecordExist==1):
-        cmd="UPDATE people SET Name="+str(Name)+"WHERE ID="+str(Id)
+        cmd="UPDATE hello SET Name="+str(Name)+"WHERE ID="+str(Id)
     else:
-        cmd="INSERT INTO people(ID,Name) Values("+str(Id)+","+str(Name)+")"
+        cmd="INSERT INTO hello(ID,Name) Values("+str(Id)+","+str(Name)+")"
     conn.execute(cmd)
     conn.commit()
     conn.close()
 id=input('enter uniqe id:')
 name=input('enter name: ')
 insertOrUpdate(id,name)
-sampleNo=0
-while(True):
-    ret,img=cam.read()
+cap=cv2.VideoCapture(0)
+count=0
+while(count<50):
+    ret,img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces=detector.detectMultiScale(gray,1.3,5);
-    for(x,y,w,h) in faces:
-        sampleNo=sampleNo+1
-        cv2.imwrite('datasets/User'+str(id)+"."+str(sampleNo)+".jpg",gray[y:y+h,x:x+w])
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        cv2.waitKey(100);
-    cv2.imshow('Face',img)
-    cv2.waitKey(1);
-    if(sampleNo>30):
-        break
 
-cam.release()
+    faces=face_casecade.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        
+        frame=gray[y:y+h,x:x+w]
+        cv2.imwrite("datasets/User."+str(id)+"."+str(count)+".jpg",frame)
+        cv2.waitKey(300)
+        cv2.imshow("CAPTURED PHOTO",frame)
+        count=count+1
+    cv2.imshow('Face recognition system capture faces',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+print('face capture for subject is completed')
+cap.release()
 cv2.destroyAllWindows()
+
